69d953dabfb6d81a49a6c4ce509c208cf93cce3f
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2025-10-13 23:13:35 +0530

    made a login in backend

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,6 @@
 const User=require('../models/User');
 const bcrypt=require('bcrypt');
-
+const jwt=require('jsonwebtoken');
 exports.signup=async(req,res)=>{
     const{name,email,password}=req.body;
     try{
@@ -10,5 +10,22 @@ exports.signup=async(req,res)=>{
         res.json({message:"user saved"});
     }catch(error){
         res.status(500).json({message:error.message});
+    }
+}
+
+
+exports.login=async(req,res)=>{
+    const{email,password}=req.body;
+    try{
+        const user=await User.findOne({email});
+        if(!user) return res.status(400).json({message:"invalid text"});
+
+        const isMatch= await bcrypt.compare(password.user.password);
+        if(!isMatch) return res.status(400).json({message:"password is not match"});
+
+        const token=jwt.sign({id:user._id},process.env.JWT_SECERT,{expiresIn:'1h'});
+        res.json({user,token})
+    }catch(error){
+        res.status(500).json({message:err.message})
     }
 }
