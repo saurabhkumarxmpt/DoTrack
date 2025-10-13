f25565c31bc491a8164536ae2954b6571de14c7a
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2025-10-13 23:20:33 +0530

    solve the problam in backend login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -20,12 +20,12 @@ exports.login=async(req,res)=>{
         const user=await User.findOne({email});
         if(!user) return res.status(400).json({message:"invalid text"});
 
-        const isMatch= await bcrypt.compare(password.user.password);
+        const isMatch= await bcrypt.compare(password,user.password);
         if(!isMatch) return res.status(400).json({message:"password is not match"});
 
         const token=jwt.sign({id:user._id},process.env.JWT_SECERT,{expiresIn:'1h'});
         res.json({user,token})
     }catch(error){
-        res.status(500).json({message:err.message})
+        res.status(500).json({message:error.message})
     }
 }
